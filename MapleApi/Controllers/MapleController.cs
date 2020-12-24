dd35e77a4c3edb7546b25b69da3a13354761f129
MikyWang <813853090@qq.com>
2020-12-24 23:21:33 +0800

    修改为.net5.0项目

---
+++ MapleApi/Controllers/MapleController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MapleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MapleController : ControllerBase
+    {
+        [HttpGet]
+        public string GetDefault()
+        {
+            return "test";
+        }
+    }
+}
